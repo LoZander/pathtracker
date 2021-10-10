0af3ad9fbcb6b2c5aab6cf478159778bc3a3046f
Alexander Dybdahl Troelsen <202008975@post.au.dk>
2021-10-10 08:02:37 +0200

    Refactored main

--- src/main/java/standard/main.java
+++ src/main/java/standard/main.java
@@ -6,14 +6,11 @@ import standard.factories.AlphaTrackerFactory;
 public class main {
 
     public static void main(String[] args) {
+        Gui gui;
         switch (args[0]) {
-            case "-alpha": consoleVariant();
-            default: consoleVariant();
+            case "-alpha": gui = new CommandGui(new TrackerImpl(new AlphaTrackerFactory()));
+            default: gui = new CommandGui(new TrackerImpl(new AlphaTrackerFactory()));
         }
-    }
-
-    private static void consoleVariant() {
-        Gui gui = new CommandGui(new TrackerImpl(new AlphaTrackerFactory()));
         gui.run();
     }
 }
