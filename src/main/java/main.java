544b4a00bebfb3ecca0ad3fb1ef9d65801681491
Alexander Troelsen <202008975@post.au.dk>
2022-01-27 03:05:25 +0100

    build

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,14 +1,16 @@
 import framework.Gui;
+import javafx.scene.media.Track;
 import standard.gui.commandGui.CommandGui;
 import standard.TrackerImpl;
 import standard.factories.AlphaTrackerFactory;
+import standard.gui.wimp.WimpGui;
 
 public class main {
 
     public static void main(String[] args) {
         Gui gui;
         switch (args[0]) {
-            case "-alpha": gui = new CommandGui(new TrackerImpl(new AlphaTrackerFactory()));
+            case "-wimp": gui = new WimpGui(new TrackerImpl(new AlphaTrackerFactory())); break;
             default: gui = new CommandGui(new TrackerImpl(new AlphaTrackerFactory()));
         }
         gui.run();
